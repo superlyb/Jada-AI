3ce59820ef91bbd24b238265ffbab89d5ef59d49
superlyb <tudouxiong@hotmail.com>
2023-05-10 14:11:17 +0200

    Update route.ts

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: NextRequest,res:NextResponse) {
     // const { context} = JSON.parse(body);
     
    
-    //const supabase = createMiddlewareSupabaseClient({ req, res });
+    const supabase = createMiddlewareSupabaseClient({ req, res });
     const { price, quantity = 1, metadata = {} } = context;
     try {
 /*       console.log("context",req)
