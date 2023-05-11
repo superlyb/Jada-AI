977363018e89049a79c5924c8413075cfeb296c0
superlyb <tudouxiong@hotmail.com>
2023-05-11 16:26:55 +0200

    Update signin.tsx

--- app/components/subscription/signin.tsx
+++ app/components/subscription/signin.tsx
@@ -20,12 +20,24 @@ const SignIn = (props: { onClose?: () => void }) => {
       return (
         <div className="flex justify-center height-screen-helper">
           <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
-            <div className="flex justify-center pb-12 ">
-              {/* <Logo width="64px" height="64px" /> } */}
-            </div>
             <div className="flex flex-col space-y-4">
              <Auth
                 supabaseClient={supabaseClient}
+                providers={['github']}
+                //redirectTo={getURL()}
+                magicLink={true}
+                appearance={{
+                  theme: ThemeSupa,
+                  variables: {
+                    default: {
+                      colors: {
+                        brand: '#404040',
+                        brandAccent: '#52525b'
+                      }
+                    }
+                  }
+                }}
+                theme="light"
                 localization={{
                   variables: {
                     sign_up: {
@@ -47,21 +59,6 @@ const SignIn = (props: { onClose?: () => void }) => {
                     },
                   },
                 }}
-                providers={['github']}
-                //redirectTo={getURL()}
-                magicLink={true}
-                appearance={{
-                  theme: ThemeSupa,
-                  variables: {
-                    default: {
-                      colors: {
-                        brand: '#404040',
-                        brandAccent: '#52525b'
-                      }
-                    }
-                  }
-                }}
-                theme="light"
               /> 
             </div>
           </div>
